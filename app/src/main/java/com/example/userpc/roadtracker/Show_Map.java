b3483b7e7d148f220243cb26d8cf3ca58d406ef4
Sourav Kanta <sourav.kanta@gmail.com>
2017-11-10 18:03:23 +0530

    Dump Database

--- app/src/main/java/com/example/userpc/roadtracker/Show_Map.java
+++ app/src/main/java/com/example/userpc/roadtracker/Show_Map.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import org.osmdroid.DefaultResourceProxyImpl;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.MapTileProviderBasic;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -13,6 +15,7 @@ import org.osmdroid.views.overlay.MyLocationOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
 import org.osmdroid.views.overlay.SimpleLocationOverlay;
+import org.osmdroid.views.overlay.TilesOverlay;
 
 import android.app.Activity;
 import android.content.Context;
@@ -77,8 +80,13 @@ public class Show_Map extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mapfragmet);
 
-		map = (MapView) findViewById(R.id.map);
+		map = (MapView) findViewById(R.id.map);/*
 		map.setTileSource(new XYTileSource("MapQuest",ResourceProxy.string.mapquest_osm, 0, 17, 256, ".jpg", new String[] {
+				"http://otile1.mqcdn.com/tiles/1.0.0/map/",
+				"http://otile2.mqcdn.com/tiles/1.0.0/map/",
+				"http://otile3.mqcdn.com/tiles/1.0.0/map/",
+				"http://otile4.mqcdn.com/tiles/1.0.0/map/"}));*/
+		map.setTileSource(new XYTileSource("OSMPublicTransport",ResourceProxy.string.public_transport, 0, 16, 256, ".png", new String[] {
 				"http://otile1.mqcdn.com/tiles/1.0.0/map/",
 				"http://otile2.mqcdn.com/tiles/1.0.0/map/",
 				"http://otile3.mqcdn.com/tiles/1.0.0/map/",
